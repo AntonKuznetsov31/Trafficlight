5d7fa79ecd3f4020b28e057021ca3279f040bc12
Anton Kuznetsov <anton.kuznetsov.mail@gmail.com>
2019-07-24 20:49:58 +0300

    1. Main app logic added. 2. Small UI fixes added.

--- Trafficlight/ViewController.swift
+++ Trafficlight/ViewController.swift
@@ -8,43 +8,88 @@
 
 import UIKit
 
+enum Trafficlight {
+    case red
+    case yellow
+    case green
+    case off
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var redLight: UIImageView!
     @IBOutlet weak var yellowLight: UIImageView!
     @IBOutlet weak var greenLight: UIImageView!
-    @IBOutlet weak var mainButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+    
+    private var currentTrafficLight = Trafficlight.off
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        startUpdateUI()
-    }
-    
-    override func viewWillLayoutSubviews() {
-        makeCircles()
-    }
-    
-    @IBAction func startButtonPressed() {
+        startButton.setTitle("Start", for: .normal)
+        createTrafficlight()
         
     }
     
-    func startUpdateUI() {
-        mainButton.setTitle("Start", for: .normal)
-        redLight.backgroundColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
-        redLight.alpha = 0.3
-        yellowLight.backgroundColor = #colorLiteral(red: 0.9994240403, green: 0.9855536819, blue: 0, alpha: 1)
-        yellowLight.alpha = 0.3
-        greenLight.backgroundColor = #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1)
-        greenLight.alpha = 0.3
+    override func viewWillLayoutSubviews() {
+        makeTrafficLightRound()
     }
     
-    func makeCircles() {
+    @IBAction func startButtonPressed() {
+        switch currentTrafficLight {
+        case .off:
+            startButton.setTitle("Next", for: .normal)
+            redLight.turnOn()
+            currentTrafficLight = .red
+        case .red:
+            redLight.turnOff()
+            yellowLight.turnOn()
+            currentTrafficLight = .yellow
+        case .yellow:
+            yellowLight.turnOff()
+            greenLight.turnOn()
+            currentTrafficLight = .green
+        case .green:
+            greenLight.turnOff()
+            redLight.turnOn()
+            currentTrafficLight = .red
+        }
+    }
+    
+    private func createTrafficlight() {
+        redLight.backgroundColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
+        redLight.alpha = 0.3
+        redLight.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        redLight.layer.borderWidth = 2
+        
+        yellowLight.backgroundColor = #colorLiteral(red: 0.9994240403, green: 0.9855536819, blue: 0, alpha: 1)
+        yellowLight.alpha = 0.3
+        yellowLight.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        yellowLight.layer.borderWidth = 2
+        
+        greenLight.backgroundColor = #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1)
+        greenLight.alpha = 0.3
+        greenLight.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        greenLight.layer.borderWidth = 2
+    }
+    
+    private func makeTrafficLightRound() {
         redLight.layer.cornerRadius = redLight.frame.size.width / 2
         redLight.clipsToBounds = true
         yellowLight.layer.cornerRadius = yellowLight.frame.size.width / 2
         yellowLight.clipsToBounds = true
         greenLight.layer.cornerRadius = greenLight.frame.size.width / 2
         greenLight.clipsToBounds = true
+    }
+}
+
+extension UIImageView {
+    func turnOff() {
+        self.alpha = 0.3
+    }
+    
+    func turnOn() {
+        self.alpha = 1
     }
 }
